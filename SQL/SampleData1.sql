98786ddc806894d61934401d4cd335337435eb28
John Maple <jmaple@bellsouth.net>
2021-12-10 22:53:13 -0600

    adding sample wish data

--- SQL/SampleData1.sql
+++ SQL/SampleData1.sql
@@ -1,16 +1,31 @@
 INSERT INTO Users (FirstName, LastName, EmailAddress, ImageURL)
-	VALUES ('John', 'Maple', 'jm@example.com', 'https://lh3.googleusercontent.com/ogw/ADea4I6GAr2cGlYwYJcMMnzuDKYC02NXtgl1_itqo7IncA=s83-c-mo');
+	VALUES ('John', 'Greenfin', 'jg@example.com', 'https://lh3.googleusercontent.com/ogw/ADea4I6GAr2cGlYwYJcMMnzuDKYC02NXtgl1_itqo7IncA=s83-c-mo');
 
 INSERT INTO ExchangePartners 
 	( CreatedById, FirstName, LastName, EmailAddress, ImageURL, Birthday, Street, City, PostalState, Zip,
 		Colors, Sizes)
-VALUES ((Select Id from Users where FirstName = 'John' AND LastName = 'Maple') , 'Andrew', 'Walton', 'aw@example.com', 'https://storage.googleapis.com/gift_tracker_images/Avatars/A.png',
+VALUES ((Select Id from Users where FirstName = 'John' AND LastName = 'Greenfin') , 'Andrew', 'Walton', 'aw@example.com', 'https://storage.googleapis.com/gift_tracker_images/Avatars/A.png',
 		'1992-01-01', '1234 Somewhere', 'Fine City', 'AZ', '12345', 'Unknown', '32 Waist, 32-33 Inseam');
+
 INSERT INTO ExchangePartners 
 	( CreatedById, FirstName, LastName, EmailAddress, ImageURL, Birthday, Street, City, PostalState, Zip,
 		Colors, Sizes)
-VALUES ((Select Id from Users where FirstName = 'John' AND LastName = 'Maple') , 'Kay', 'Walton', 'kw@example.com', 'https://storage.googleapis.com/gift_tracker_images/Avatars/K.png',
+VALUES ((Select Id from Users where FirstName = 'John' AND LastName = 'Greenfin') , 'Kay', 'Walton', 'kw@example.com', 'https://storage.googleapis.com/gift_tracker_images/Avatars/K.png',
 		'1970-01-01', '1234 SomewhereElse', 'Another Fine City', 'AL', '54321', 'Unknown', 'Small');
+
 INSERT INTO Occasions ( OccasionCreatorId, OccasionName, OccasionDate, OccasionBudget)
-VALUES ((Select Id from Users where FirstName = 'John' AND LastName = 'Maple'), 'Christmas', '12-25-2021', '500.00');
+VALUES ((Select Id from Users where FirstName = 'John' AND LastName = 'Greenfin'), 'Christmas', '12-25-2021', '500.00');
+
+INSERT INTO WishListItems ( OccasionId, OwnerId, Description, ItemURL )
+VALUES ((SELECT Id FROM Occasions WHERE OccasionDate = '12-25-2021'), (SELECT Id FROM ExchangePartners WHERE FirstName = 'Andrew' AND LastName = 'Walton'),
+		'Any film on Blu Ray or DVD from this site: https://www.zipporah.com/', 'https://www.zipporah.com/');
+
+INSERT INTO WishListItems ( OccasionId, OwnerId, Description, ItemURL )
+VALUES ((SELECT Id FROM Occasions WHERE OccasionDate = '12-25-2021'), (SELECT Id FROM ExchangePartners WHERE FirstName = 'Andrew' AND LastName = 'Walton'),
+		'Anything to do with cooking, spices, recipes or book written by a chef', '');
+
+INSERT INTO WishListItems ( OccasionId, OwnerId, Description, ItemURL )
+VALUES ((SELECT Id FROM Occasions WHERE OccasionDate = '12-25-2021'), (SELECT Id FROM ExchangePartners WHERE FirstName = 'Andrew' AND LastName = 'Walton'),
+		'Golf shorts (not too long over the knee... Target sells good ones), black jeans or dark chinos, 32 waist. length 30-32. Tapered or slim', '');
+
 
