9da8647597974310700e54e20ea154d2250418fa
John Maple <jmaple@bellsouth.net>
2021-12-13 23:44:28 -0600

    beginning repository for Interests

---
+++ GiftTracker/DataAccess/InterestRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using GiftTracker.Models;
+
+namespace GiftTracker.DataAccess
+{
+    public class InterestRepository
+    {
+        readonly string _connectionString;
+        public InterestRepository(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("GifTracker");
+        }
+
+        internal IEnumerable<Interest> GetAllInterests()
+        {
+            using var db = new SqlConnection(_connectionString);
+            var interests = db.Query<Interest>(@"SELECT * FROM Interests");
+            return interests;
+        }
+
+        internal Interest GetInterestById(Guid interestId)
+        {
+            using var db = new SqlConnection(_connectionString);
+            var sql = @"SELECT * FROM Interests
+                        WHERE Id = @Id";
+            var result = db.QueryFirstOrDefault<Interest>(sql, new { Id = interestId });
+            return result;
+        }
+
+        internal IEnumerable<Interest> GetInterestsByPartnerID(Guid partnerId)
+        {
+            using var db = new SqlConnection(_connectionString);
+            var sql = @"SELECT * FROM Interests
+                        WHERE ExchangePartnerId = @Id";
+            var parameter = new
+            {
+                Id = partnerId
+            };
+
+            var result = db.Query<Interest>(sql, parameter);
+            return result;
+
+        }
+    }
+}
