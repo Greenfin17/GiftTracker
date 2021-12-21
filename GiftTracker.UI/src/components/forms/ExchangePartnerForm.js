7a6d8d5b2f7c9100461188f81d5a7bb45b2692f1
John Maple <jmaple@bellsouth.net>
2021-12-20 21:30:50 -0600

    re-arranging style sheets

--- GiftTracker.UI/src/components/forms/ExchangePartnerForm.js
+++ GiftTracker.UI/src/components/forms/ExchangePartnerForm.js
@@ -83,27 +83,29 @@ const ExchangePartnerForm = ({
       <div className='form-heading'>Edit Exchange Partner Profile
         <span className='x-out' onClick={closeModal}>x</span>
       </div>
-      <label className='input-label' htmlFor='firstName'>First Name</label>
-        <input className='form-input' type='text' name='firstName' value={partnerProfile.firstName}
-              label='firstName' onChange={handleChange} />
-      <label className='input-label' htmlFor='lastName'>Last Name</label>
-        <input className='form-input' type='text' name='lastName' value={partnerProfile.lastName}
-              label='lastName' onChange={handleChange} />
-      <label className='input-label' htmlFor='emailAddress'>Email Address</label>
-        <input className='form-input' type='text' name='emailAddress' value={partnerProfile.emailAddress}
-              label='emailAddress' onChange={handleChange} />
-      <label className='input-label' htmlFor='imageURL'>Profile Image URL</label>
-        <input className='form-input' type='text' name='imageURL' value={partnerProfile.imageURL}
-              label='imageURL' onChange={handleChange} />
-      <label className='input-label' htmlFor='birthday'>Birthday</label>
-        <input className='form-input' type='date' name='birthday' value={partnerProfile.birthday.substring(0,10)}
-              label='birthday' onChange={handleChange} />
-      <label className='input-label' htmlFor='colors'>Favorite Colors</label>
-        <input className='form-input' type='text' name='colors' value={partnerProfile.colors}
-              label='colors' onChange={handleChange} />
-      <label className='input-label' htmlFor='sizes'>Sizes</label>
-        <input className='form-input' type='text' name='sizes' value={partnerProfile.sizes}
-              label='sizes' onChange={handleChange} />
+      <div className='form-group'>
+        <label className='input-label' htmlFor='firstName'>First Name</label>
+            <span className='partner-input-span'><input className='form-input' type='text' name='firstName' value={partnerProfile.firstName}
+                  label='firstName' onChange={handleChange} /></span>
+        <label className='input-label' htmlFor='lastName'>Last Name</label>
+          <input className='form-input' type='text' name='lastName' value={partnerProfile.lastName}
+                label='lastName' onChange={handleChange} />
+        <label className='input-label' htmlFor='emailAddress'>Email Address</label>
+          <input className='form-input' type='text' name='emailAddress' value={partnerProfile.emailAddress}
+                label='emailAddress' onChange={handleChange} />
+        <label className='input-label' htmlFor='imageURL'>Profile Image URL</label>
+          <input className='form-input' type='text' name='imageURL' value={partnerProfile.imageURL}
+                label='imageURL' onChange={handleChange} />
+        <label className='input-label' htmlFor='birthday'>Birthday</label>
+          <input className='form-input' type='date' name='birthday' value={partnerProfile.birthday.substring(0,10)}
+                label='birthday' onChange={handleChange} />
+        <label className='input-label' htmlFor='colors'>Favorite Colors</label>
+          <input className='form-input' type='text' name='colors' value={partnerProfile.colors}
+                label='colors' onChange={handleChange} />
+        <label className='input-label' htmlFor='sizes'>Sizes</label>
+          <input className='form-input' type='text' name='sizes' value={partnerProfile.sizes}
+                label='sizes' onChange={handleChange} />
+      </div>
       <div className='button-div'>
         <button className='close-button' onClick={closeModal}>Close</button>
         <button className='submit-button' onClick={handleSubmit}>Submit</button>
